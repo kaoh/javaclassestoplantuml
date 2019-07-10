e573a6f94e5d0e65445762bd943dc17fce7e8e57
Karsten Ohme <k_o_@users.sourceforge.net>
2019-07-10 15:13:10 +0200

    fix

--- src/main/java/de/ohmesoftware/classestoplantuml/Converter.java
+++ src/main/java/de/ohmesoftware/classestoplantuml/Converter.java
@@ -136,10 +136,6 @@ public class Converter {
                         printWriter.println();
                     }
                 }
-                else {
-                    // ignore interfaces
-                    continue;
-                }
                 printWriter.println("}");
                 associations.forEach(printWriter::println);
                 // add inheritance
